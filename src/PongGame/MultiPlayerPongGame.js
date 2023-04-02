fcfb05406031c471780ce2d55942ac954bbc0ffa
Robby Linson <linsonr@tcd.ie>
2023-04-02 23:48:32 +0100

    New classes made, pong and MultiPlayerPongGame bro

---
+++ src/PongGame/MultiPlayerPongGame.js
@@ -0,0 +1,188 @@
+//File: MultiPlayerPongGame.js
+
+"use strict";
+
+function MultiPlayerPongGame() {
+    this.kBallSprite = "assets/snake_sprites/apple.png";
+    this.kSnakeSprite = "assets/snake_sprites/paddle.png";
+    this.kSnakeSegmentSprite = "assets/snake_sprites/body_vertical.png";
+    this.kBgSprite = "assets/snake_sprites/snake_bg.png";
+    this.kEnemySnake = "assets/snake_sprites/enemyPaddle.png";
+    this.kEnemySnakeSegmentSprite = "assets/snake_sprites/red_body_vertical.png";
+
+    this.mCamera = null;
+
+    this.mMsg = null;
+    this.mInputMsg = null;
+    this.mFPSMsg = null;
+    this.mScoreMsg = null;
+
+    this.mPaddle = null;
+    this.mPlayer2Paddle = null;
+    this.mBall = null;
+    this.mSegments = [];
+    this.mTail = null;
+    this.mNewAllowed = true;
+    this.mGridSegments = null;
+    this.mBg = null;
+}
+gEngine.Core.inheritPrototype(PongGame, Scene)
+
+PongGame.prototype.loadScene = function(){
+    gEngine.Textures.loadTexture(this.kBallSprite);
+    gEngine.Textures.loadTexture(this.kSnakeSegmentSprite);
+    gEngine.Textures.loadTexture(this.kEnemySnakeSegmentSprite);
+    gEngine.Textures.loadTexture(this.kSnakeSprite);
+    gEngine.Textures.loadTexture(this.kEnemySnake);
+    gEngine.Textures.loadTexture(this.kBgSprite);
+};
+PongGame.prototype.unloadScene = function(){
+    gEngine.Textures.unloadTexture(this.kBallSprite);
+    gEngine.Textures.unloadTexture(this.kSnakeSegmentSprite);
+    gEngine.Textures.unloadTexture(this.kEnemySnakeSegmentSprite);
+    gEngine.Textures.unloadTexture(this.kSnakeSprite);
+    gEngine.Textures.unloadTexture(this.kEnemySnake);
+};
+
+PongGame.prototype.initialize = function () {
+    // Step A: set up the cameras
+    this.mCamera = new Camera(
+        vec2.fromValues(128, 128), // position of the camera
+        256,                       // width of camera
+        [0, 0, 500, 500]           // viewport (orgX, orgY, width, height)
+    );
+    this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
+            // sets the background to gray
+
+    this.mBall = new Ball(this.kBallSprite);
+    this.mPaddle = new Paddle(this.kSnakeSprite);
+    this.mPlayer2Paddle = new Player2Paddle(this.kEnemySnake, this.mBall)
+    this.mTail = this.mPaddle;
+    //MORE THINGS HERE
+
+    this.mBg = new TextureRenderable(this.kBgSprite);
+    this.mBg.setColor([1,1,1,0]);
+    this.mBg.getXform().setSize(256,256);
+    this.mBg.getXform().setPosition(128,128);
+
+    this.mMsg = new FontRenderable("Hello Pong! [R] to reset");
+    this.mMsg.setColor([0, 0, 0, 1]);
+    this.mMsg.getXform().setPosition(10, 10);
+    this.mMsg.setTextHeight(10);
+
+    this.mInputMsg = new FontRenderable("Status Message");
+    this.mInputMsg.setColor([0, 0, 0, 1]);
+    this.mInputMsg.getXform().setPosition(10, 20);
+    this.mInputMsg.setTextHeight(10);
+
+    this.mFPSMsg = new FontRenderable("FPS msg");
+    this.mFPSMsg.setColor([0, 0, 0, 1]);
+    this.mFPSMsg.getXform().setPosition(1, 10);
+    this.mFPSMsg.setTextHeight(10);
+
+    this.mScoreMsg = new FontRenderable("1");
+    this.mScoreMsg.setColor([0, 0, 0, 1]);
+    this.mScoreMsg.getXform().setPosition(10, 40);
+    this.mScoreMsg.setTextHeight(10);
+
+    this.mPlayerScoreMsg = new FontRenderable("Player score msg");
+    // this.mPlayerScoreMsg.setColor([0, 0, 0, 0]);
+    this.mPlayerScoreMsg.getXform().setPosition(87, 57);
+    this.mPlayerScoreMsg.setTextHeight(10);
+    
+    this.mEnemyScoreMsg = new FontRenderable("Enemy score msg");
+    // this.mEnemyScoreMsg.setColor([0, 0, 0, 0]);
+    this.mEnemyScoreMsg.getXform().setPosition(90, 203);
+    this.mEnemyScoreMsg.setTextHeight(10);
+    
+    this.mRoundOverMsg = new FontRenderable("Round over msg");
+    // this.mRoundOverMsg.setColor([0, 0, 0, 0]);
+    this.mRoundOverMsg.getXform().setPosition(20, 140);
+    this.mRoundOverMsg.setTextHeight(10);
+    
+    this.mPausedMsg = new FontRenderable("Paused game msg");
+    // this.mPausedMsg.setColor([0, 0, 0, 0]);
+    this.mPausedMsg.getXform().setPosition(88, 137);
+    this.mPausedMsg.setTextHeight(10);
+
+
+    this.mFPSMsg.getXform().setPosition(10, 30);
+    this.mFPSMsg.setTextHeight(10);
+
+    // this.mSegments.push(this.mPaddle);
+    // this.mFrameCounter = 0;
+
+    this.mGridSegments = Array.from({length: 16}, () =>
+        Array.from({length: 16}, () => false)
+    );
+
+    gEngine.DefaultResources.setGlobalAmbientIntensity(1);
+    gEngine.DefaultResources.setGlobalAmbientColor([1, 1, 1, 1]);
+};
+
+// PongGame.prototype.makeSegment = function(){
+//     let newSegment = new PaddleSegment(this.kSnakeSegmentSprite, this.mTail);
+//     this.mTail = newSegment;
+//     this.mSegments.push(newSegment);
+// }
+
+PongGame.prototype.draw = function () {
+    // Step A: clear the canvas
+    gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
+
+    // Step  B: Activate the drawing Camera
+    this.mCamera.setupViewProjection();
+
+    // Step  C: Draw everything
+    this.mBg.draw(this.mCamera);
+    this.mBall.draw(this.mCamera);
+    this.mPaddle.draw(this.mCamera);
+    this.mPlayer2Paddle.draw(this.mCamera);
+    
+   // this.mSegments.forEach(segment => segment.draw(this.mCamera));
+
+    // this.mMsg.draw(this.mCamera);
+    // this.mInputMsg.draw(this.mCamera);
+    // this.mFPSMsg.draw(this.mCamera);
+    // this.mScoreMsg.draw(this.mCamera);
+    this.mPlayerScoreMsg.draw(this.mCamera);
+    this.mEnemyScoreMsg.draw(this.mCamera);
+    this.mRoundOverMsg.draw(this.mCamera);
+    this.mPausedMsg.draw(this.mCamera);
+};
+
+PongGame.prototype.update = function(){
+    let msg = "Last pressed command: ";
+    let fpsMsg = "Frame Counter: ";
+    let scoreMsg = "Collisions: ";
+    let playerScoreMsg = "Player score: ";
+    let enemyScoreMsg = "Enemy score: ";
+    let roundOverMsg = "Round over. Press A or D to continue.";
+    let pausedGameMsg = "Game is Paused.";
+    this.mBall.update();
+    this.mBall.collide(this.mPaddle, this.mPlayer2Paddle)
+    this.mPlayer2Paddle.update();
+    this.mPaddle.update();
+
+    if(gEngine.Input.isKeyClicked(gEngine.Input.keys.P)) { 
+        this.mBall.pauseGame ^= 1;
+    } 
+        // this.mScoreMsg.setText(scoreMsg + this.mBall.collideCount);
+
+    this.mFrameCounter++;
+    this.mEnemyScoreMsg.setText(enemyScoreMsg + this.mBall.enemyScore);
+    this.mPlayerScoreMsg.setText(playerScoreMsg + this.mBall.playerScore);
+    // this.mInputMsg.setText(msg + this.mPaddle.mLastLetter);
+    // this.mFPSMsg.setText(fpsMsg + this.mPaddle.mFrameCounter);    
+    if(this.mBall.roundOver == 1) {
+        this.mRoundOverMsg.setText(roundOverMsg);
+    } else {
+        this.mRoundOverMsg.setText("");
+    }
+    if(this.mBall.pauseGame == 1 && this.mBall.roundOver == 0) {
+        this.mPausedMsg.setText(pausedGameMsg);
+    } else {
+        this.mPausedMsg.setText("");
+    }
+    
+};
---
+++ src/PongGame/Objects/Player2Paddle.js
@@ -0,0 +1,86 @@
+//File: EnemyPaddle.js
+"use strict";
+
+
+
+function EnemyPaddle(spriteTexture, ball){
+    this.mEnemyPaddle = new TextureRenderable(spriteTexture);
+    this.mBall = ball
+    this.mEnemyPaddle.setColor([1,1,1,0]);
+    let initCoords = this.getSquareCoords(8,16);
+    this.mEnemyPaddle.getXform().setPosition(initCoords[0],initCoords[1]);
+    this.mEnemyPaddle.getXform().setSize(14,58);
+    
+    this.mLastLetter="A";
+    this.mFrameCounter = 0;
+    this.mFrameUpdateInterval = 10;
+    GameObject.call(this, this.mEnemyPaddle)
+
+    this.setSpeed(16/10);
+    this.mEnemyPaddle.getXform().setRotationInDegree(90);
+    vec2.set(this.getCurrentFrontDir(), -1, 0);
+    this.wiggleTimer = 0;
+}
+gEngine.Core.inheritPrototype(EnemyPaddle, GameObject);
+
+EnemyPaddle.prototype.getSquareCoords = function (xSquare, ySquare){
+    let coordsArray = [];
+
+    // 1-indexed, 16 * 16 grid, gets the middle of squares
+    coordsArray.push((xSquare * 16) - 8);
+    coordsArray.push((ySquare * 16) - 8);
+    return coordsArray;
+}
+
+EnemyPaddle.prototype.takeInput = function () {
+    var xform = this.getXform();
+
+    if (gEngine.Input.isKeyPressed(gEngine.Input.keys.Left)) {
+        this.mLastLetter = "A";
+    }
+    if (gEngine.Input.isKeyPressed(gEngine.Input.keys.Right)) {
+        this.mLastLetter = "D";
+    }
+};
+
+EnemyPaddle.prototype.update = function(){
+    if(gEngine.Input.isKeyClicked(gEngine.Input.keys.Escape)) { 
+        this.mBall.pauseGame ^= 1;
+    } 
+    if(this.mBall.pauseGame == 0) {
+        GameObject.prototype.update.call(this);
+        if(this.mPaddle.getXform().getXPos() <= 28 && this.mLastLetter == 'A')
+        {
+            this.setSpeed(0);
+        }
+    else  if(this.mPaddle.getXform().getXPos() >= 228 && this.mLastLetter == 'D')
+        {
+            this.setSpeed(0);
+        }
+        else{
+            this.setSpeed(16/10);
+        }
+        this.takeInput();
+        this.mFrameCounter++;
+        if(this.mFrameCounter == this.mFrameUpdateInterval){
+            this.mFrameCounter = 0;
+            // if(xform.getXPos()>=248 || xform.getXpos()<=8)
+            // {
+            //     vec2.set(fdir, 0,0);
+            // }
+            var xform = this.getXform();
+            var fdir = this.getCurrentFrontDir();
+            
+                switch(this.mLastLetter){
+                    case 'A':
+                        xform.setRotationInDegree(90);
+                        vec2.set(fdir, -1, 0);
+                        break;
+                    case 'D':
+                        xform.setRotationInDegree(270);
+                        vec2.set(fdir, 1, 0);
+                        break;
+            }
+        }
+    }
+};
